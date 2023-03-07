c634cf5a705da12aa8070e417d7ff7668bfbb2a8
fjbarragans <franciscojbarragans@gmail.com>
2023-03-07 10:21:07 -0600

    band name generator

---
+++ Day1.r
@@ -0,0 +1,6 @@
+#Band name generator start
+cat("Hello, I am here to create your restaurant's name.\n")
+city <- readline(prompt = "Where were you born? ")
+food <- readline(prompt = "What is your favourite food? ")
+cat("Your restaurant should be named:", city, food, "\n")
+
